90a71f3ef1e306b3f52371e6e6f5122c2d90ae04
Max <tools.shiny.01@icloud.com>
2026-03-19 13:59:05 -0700

    adding backend

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+# app.py
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+SCHEDULE_DATA = {
+    "1": {
+        "id": "1",
+        "commands": ['"$led_set_rgb 255 0 0" "$led turn_off"'],
+        "color": "#33FF33",
+        "day": 0,
+        "start_time": 540,
+        "end_time": 600
+    },
+    "2": {
+        "id": "2",
+        "commands": ['"$led_set_rgb 0 255 0" "$led turn_on"'],
+        "color": "#33FF33",
+        "day": 1,
+        "start_time": 600,
+        "end_time": 660
+    }
+}
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/schedule/json')
+def schedule_json():
+    return jsonify(SCHEDULE_DATA)
 
 if __name__ == '__main__':
     app.run(debug=True)
